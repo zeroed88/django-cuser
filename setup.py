6be87da431bbfd6ab79f9c58165a454cb8975f92
Alireza Savand <alireza.savand@gmail.com>
2012-05-14 15:57:10 +0400

    update to read README and some classifiers

--- setup.py
+++ setup.py
@@ -4,14 +4,16 @@ setup(
     name='django-cuser',
     version=".".join(map(str, __import__("cuser").__version__)),
     description='Middleware to make user information always available.',
-    long_description='README',
+    long_description=open('README').read(),
     author='Alireza Savand',
     author_email='alireza.savand@gmail.com',
     url='https://github.com/Alir3z4/django-cuser',
     packages=find_packages(exclude=["django_cuser"]),
     install_requires=['django>=1.2'],
     classifiers=[
+        "Programming Language :: Python",
         "Framework :: Django",
+        "Environment :: Web Environment",
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
         "Topic :: Software Development"
