d31f8273bfd9dfe48ca63402fc8b6d37dc7c98a0
daniell <daniell@aptivate.org>
2014-04-28 10:29:08 +0100

    Fix bug with getting custom user model

--- cuser/tests.py
+++ cuser/tests.py
@@ -37,7 +37,7 @@ from django.test import TestCase
 
 try:
     from django.contrib.auth import get_user_model
-    User = get_user_model
+    User = get_user_model()
 except ImportError:
     from django.contrib.auth.models import User
 
