aae30648d7e61994d73651bfccd639c278d92977
Alireza Savand <alireza.savand@gmail.com>
2014-09-28 07:59:01 +0400

    Code cleanup on tests module

--- cuser/tests.py
+++ cuser/tests.py
@@ -1,13 +1,13 @@
 import sys
 
 from django.conf.urls import patterns
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.http import HttpResponse
 from django.test import TestCase
 
 from cuser.fields import CurrentUserField
 from cuser.middleware import CuserMiddleware
-from cuser.compat import get_user_model
 
 
 User = get_user_model()
@@ -15,10 +15,13 @@ User = get_user_model()
 
 class TestModel(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    creator = CurrentUserField(add_only=True,
-                               blank=True,
-                               null=True,
-                               related_name="tests_created")
+    creator = CurrentUserField(
+        add_only=True,
+        blank=True,
+        null=True,
+        related_name="tests_created"
+    )
+
 
 class TestModel2(models.Model):
     name = models.CharField(max_length=100, unique=True)
@@ -27,18 +30,22 @@ class TestModel2(models.Model):
 
 def test_view(request):
     user = CuserMiddleware.get_user()
+
     return HttpResponse(user and user.username or "")
 
 
-    pass
-
 class CuserTestCase(TestCase):
-    urls = patterns('',
+    urls = patterns(
+        '',
         (r"^test-view/", test_view, {}, "test-view"),
     )
 
     def setUp(self):
-        self.user = User.objects.create_user("test", "test@example.com", "test")
+        self.user = User.objects.create_user(
+            username="test",
+            email="test@example.com",
+            password="test"
+        )
 
     def test_cuser_middleware(self):
         response = self.client.get("/test-view/")
@@ -46,8 +53,10 @@ class CuserTestCase(TestCase):
             self.assertEqual(response.content, "")
         else:
             self.assertEqual(response.content, b"")
+
         self.client.login(username="test", password="test")
         response = self.client.get("/test-view/")
+
         if sys.version < '3':
             self.assertEqual(response.content, "test")
         else:
@@ -59,12 +68,12 @@ class CuserTestCase(TestCase):
         TestModel.objects.create(name="TEST")
         CuserMiddleware.del_user()
         test_instance = TestModel.objects.get(name="TEST")
+
         self.assertEqual(test_instance.creator, user)
 
     def test_current_user_field_with_no_active_user(self):
         CuserMiddleware.del_user()
         TestModel.objects.create(name="TEST")
         test_instance = TestModel.objects.get(name="TEST")
+
         self.assertEqual(test_instance.creator, None)
-
-
